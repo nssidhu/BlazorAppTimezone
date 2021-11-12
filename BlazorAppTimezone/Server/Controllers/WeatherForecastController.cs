7e53a8974eee5f38abb6a67e19a34c3744a462ec
Naunihal Singh Sidhu <nssidhu@outlook.com>
2021-11-11 23:07:08 -0600

    saving

--- BlazorAppTimezone/Server/Controllers/WeatherForecastController.cs
+++ BlazorAppTimezone/Server/Controllers/WeatherForecastController.cs
@@ -23,35 +23,42 @@ namespace BlazorAppTimezone.Server.Controllers
 
         [Route("GetTimeZonetime/{TimeZone}")]
         [HttpGet]
-        public TimezoneDetails GetTimeZonetime(string TimeZone)
+        public IActionResult GetTimeZonetime(string TimeZone)
         {
-            var _timeZone = Uri.UnescapeDataString(TimeZone);
-            //TimeZone = "America/Chicago";
-            TimeZoneInfo timezoneID = TimeZoneInfo.FindSystemTimeZoneById(_timeZone);
+            try
+            {
+                var _timeZone = Uri.UnescapeDataString(TimeZone);
+                //TimeZone = "America/Chicago";
+                TimeZoneInfo timezoneID = TimeZoneInfo.FindSystemTimeZoneById(_timeZone);
 
-            DateTimeOffset timeAtLcoation = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timezoneID);
-
-
-            var dtoffsettime = timeAtLcoation;
-            Console.WriteLine(dtoffsettime.ToString("yyyy-MM-dd HH:mm:ss tt \"GMT\"zzz"));
-
-            DateTimeOffset val = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, timezoneID.StandardName);
-            DateTimeOffset val2 = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, timezoneID.Id);
-
-            string Windowstimezone = "";
-            TimeZoneInfo.TryConvertIanaIdToWindowsId("America/Chicago", out Windowstimezone);
+                DateTimeOffset timeAtLcoation = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timezoneID);
 
 
-            var timezoneDetails = new TimezoneDetails();
-            timezoneDetails.TimeAtTimezone = TimeZone;
-            timezoneDetails.TimezoneID = timezoneID.Id;
-            timezoneDetails.TimeZoneDisplayName = timezoneID.DisplayName;
-            timezoneDetails.TimeAtTimezone = timeAtLcoation.ToString("yyyy-MM-dd HH:mm:ss tt \"GMT\"zzz"); ;
-            timezoneDetails.UTCTime = DateTime.UtcNow.ToString();
-            timezoneDetails.ConvertedTimezone = Windowstimezone;
-            return timezoneDetails;
+                var dtoffsettime = timeAtLcoation;
+                Console.WriteLine(dtoffsettime.ToString("yyyy-MM-dd HH:mm:ss tt \"GMT\"zzz"));
+
+                DateTimeOffset val = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, timezoneID.StandardName);
+                DateTimeOffset val2 = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, timezoneID.Id);
+
+                string Windowstimezone = "";
+                TimeZoneInfo.TryConvertIanaIdToWindowsId("America/Chicago", out Windowstimezone);
 
 
+                var timezoneDetails = new TimezoneDetails();
+                timezoneDetails.TimeAtTimezone = TimeZone;
+                timezoneDetails.TimezoneID = timezoneID.Id;
+                timezoneDetails.TimeZoneDisplayName = timezoneID.DisplayName;
+                timezoneDetails.TimeAtTimezone = timeAtLcoation.ToString("yyyy-MM-dd HH:mm:ss tt \"GMT\"zzz"); ;
+                timezoneDetails.UTCTime = DateTime.UtcNow.ToString();
+                timezoneDetails.ConvertedTimezone = Windowstimezone;
+                return Ok(timezoneDetails);
+            }
+            catch (Exception e)
+            {
+               return BadRequest(e.GetBaseException().ToString());
+            }
+          
+           
 
         }
     }
--- BlazorAppTimezone/Shared/WeatherForecast.cs
+++ BlazorAppTimezone/Shared/WeatherForecast.cs
@@ -13,14 +13,14 @@ namespace BlazorAppTimezone.Shared
 
     public class TimezoneDetails
     {
-        public string TimeZoneName { get; set; }
-        public string TimezoneID { get; set; }
-        public string TimeZoneDisplayName { get; set; }
+        public string? TimeZoneName { get; set; }
+        public string? TimezoneID { get; set; }
+        public string? TimeZoneDisplayName { get; set; }
 
-        public string TimeAtTimezone { get; set; }
+        public string? TimeAtTimezone { get; set; }
 
-        public string UTCTime { get; set; }
+        public string? UTCTime { get; set; }
 
-        public string ConvertedTimezone { get; set; }
+        public string? ConvertedTimezone { get; set; }
     }
 }
